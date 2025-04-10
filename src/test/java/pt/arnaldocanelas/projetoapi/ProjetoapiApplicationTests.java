89017364e3f2a48da3bcc2c4588dd3890be1c66a
amac81 <arnaldocanelas@gmail.com>
2025-04-10 19:18:27 +0100

    projet update

---
+++ src/test/java/pt/arnaldocanelas/projetoapi/ProjetoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.arnaldocanelas.projetoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
